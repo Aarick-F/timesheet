b165a93a894afa7e060a79de42941d3537260b8c
Aarick-F <zeflyndesign@gmail.com>
2018-03-24 14:15:10 -0400

    added database functionality, sets values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,4 +10,43 @@ $(document).ready(() => {
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+
+  // button on-click logic
+  $("#addEmployee").on("click", function() {
+    // pull the values from the input fields
+    // and assign them to variables
+    let name = $("#ee-name").val().trim();
+    let role = $("#ee-role").val().trim();
+    let startDate = moment($("#start-date").val().trim()).format("DD MM gg");
+    let rate = $("#ee-rate").val().trim();
+
+    let today = moment().format("X");
+    let monthsWorked = moment().diff(moment($("#start-date").val().trim(), "DD/MM/YY"), "Months");
+    let totalBilled = rate * monthsWorked;
+
+    // console.log(name);
+    // console.log(role);
+    // console.log(startDate);
+    // console.log(rate);
+    // console.log(monthsWorked);
+    // console.log(totalBilled);
+
+    database.ref().push({
+      name: name,
+      role: role,
+      startDate: startDate,
+      rate: rate,
+      monthsWorked: monthsWorked,
+      totalBilled: totalBilled
+    });
+
+  });
+
+  database.ref().on("value", function(snapshot) {
+    console.log(snapshot);
+    // get data from the most recently added employee from the database
+    // create html elements via jQuery and append those along with the
+    // data to the table element.
+  });
+
 });
